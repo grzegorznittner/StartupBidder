d3b7506dbdd8dea745f63637aa5e7a816072e0b8
John Arley Burns <johnarleyburns@gmail.com>
2012-03-18 23:38:24 +0100

    new listing submitted page added

---
+++ html5-boilerplate/js/modules/newlistingsubmittedpage.js
@@ -0,0 +1,32 @@
+function NewListingSubmittedClass() {
+    var base = new NewListingBaseClass();
+    this.base = base;
+}
+pl.implement(NewListingSubmittedClass, {
+    load: function() {
+        var self = this,
+            completeFunc = function(json) {
+                var listing = json && json.listing ? json.listing : {},
+                    header = new HeaderClass();
+                header.setLogin(json);
+                self.base.store(listing);
+                self.display();
+            };
+        ajax = new AjaxClass('/listings/create', 'newlistingmsg', completeFunc);
+        ajax.setPost();
+        ajax.call();
+    },
+    display: function() {
+    }
+});
+
+function NewListingPageClass() {};
+pl.implement(NewListingPageClass, {
+    loadPage: function() {
+        var newlisting = new NewListingSubmittedClass();
+        newlisting.load();
+    }
+});
+
+(new NewListingPageClass()).loadPage();
+
