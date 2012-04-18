9ae27abf6e53d5eea711b51d176843af0a088f4f
Grzegorz Nittner <grzegorz.nittner@gmail.com>
2012-04-18 16:25:35 +0200

    Added echos to m4 build script.

--- html5-boilerplate/m4/build.sh
+++ html5-boilerplate/m4/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+echo "Building HTML pages from templates using M4..."
 PAGES=`find . -name '*page.m4'|sed 's/^[^a-zA-Z0-9_-]*//;s/[.]m4$//'`
 for i in $PAGES; do m4 ${i}.m4 > ../${i}.html; done
+echo "... done!"
 
